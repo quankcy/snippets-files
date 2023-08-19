043537de7b638699a6a44e09ce7f2876889ad3ed
Karol Poznakowski <karol.poznakowski@gmail.com>
2023-08-20 01:39:41 +0200

    CORS config

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://snippets-ten.vercel.app"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
