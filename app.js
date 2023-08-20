9eb1f6a7cd01c852e3c37d4fe9c05cf89e4a02b9
Karol Poznakowski <karol.poznakowski@gmail.com>
2023-08-20 02:01:35 +0200

    CORS config

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = 3003
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://snippets-ten.vercel.app"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
@@ -16,4 +17,11 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
+app.use(function (req, res, next) {
+    // var filename = path.basename(req.url);
+    // var extension = path.extname(filename);
+    // if (extension === '.css')
+    console.log("The file " + req.url + " was requested.");
+    next();
+});
 app.use(express.static('public'))
