ca374070104eb9992aaa7f782261a315f1bd43cf
Karol Poznakowski <karol.poznakowski@gmail.com>
2023-08-20 22:32:55 +0200

    Static use /snippet

--- app.js
+++ app.js
@@ -1,27 +1,54 @@
 const express = require('express')
-const path = require('path');
+const morgan = require("morgan");
+// const path = require('path');
+// const fs = require('fs');
+
 const app = express()
 const port = 3003
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://snippets-ten.vercel.app"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "https://snippets-ten.vercel.app"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
+
+app.use(morgan('combined'))
 
 app.get('/', (req, res) => {
     res.send('<html><body>Hello World!</body></html>')
 })
 
+// app.get('/snippet?test=java/codingBat/warmup-1/Diff21Test.java', (req, res) => {
+//     let readFileSync = fs.readFileSync('./public/java/codingBat/warmup-1/Diff21Test.java', 'utf-8');
+//     // console.log(readFileSync)
+//     res.set('Content-Type', 'text/plain')
+//     res.send(readFileSync)
+// })
+
+// app.get('/java/codingBat/warmup-1/Diff21Test.java', (req, res) => {
+//     let readFileSync = fs.readFileSync('./public/java/codingBat/warmup-1/Diff21Test.java', 'utf-8');
+//     // console.log(readFileSync)
+//     // // res.set('Content-Type', 'application/json')
+//     res.json({'data': readFileSync})
+//     // res.json({'data': "asd"})
+// })
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
-app.use(function (req, res, next) {
-    // var filename = path.basename(req.url);
-    // var extension = path.extname(filename);
-    // if (extension === '.css')
-    console.log("The file " + req.url + " was requested.");
-    next();
-});
-app.use(express.static('public'))
+// app.use(function (req, res, next) {
+//     // var filename = path.basename(req.url);
+//     // var extension = path.extname(filename);
+//     // if (extension === '.css')
+//     res.set('Content-Type', 'text/plain')
+//     console.log("The file " + req.url + " was requested.");
+//     console.log("REQUEST-START")
+//     console.log(req)
+//     console.log("REQUEST-END")
+//     console.log("RESPONSE-START")
+//     console.log(res)
+//     console.log("RESPONSE-END")
+//     next();
+// });
+app.use('/snippet', express.static('snippet'))
