c2bdd603190def39a56c1812cb8852ebafe1e6b6
Karol Poznakowski <karol.poznakowski@gmail.com>
2023-08-26 09:32:17 +0200

    SleepInTest.java

---
+++ .vercel/output/static/java/codingbat/warmup-1/SleepInTest.java
@@ -0,0 +1,13 @@
+package org.example.codingbat;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Diff21Test {
+// https://codingbat.com/prob/p187868
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        return !weekday || vacation;
+    }
+
+}
